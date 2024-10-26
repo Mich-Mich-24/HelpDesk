86d2020cac8746bbf37747b55f169176f18fc26e
Mich-Mich-24 <michelledibucks24@gmail.com>
2024-10-26 10:55:32 +0200

    Add project files.

---
+++ HelpDesk/Controllers/UsersController.cs
@@ -0,0 +1,130 @@
+﻿using HelpDesk.Data;
+using HelpDesk.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace HelpDesk.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+
+        public UsersController(ApplicationDbContext context, UserManager<ApplicationUser>userManager, RoleManager<IdentityRole> roleManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+
+            
+        }
+        // GET: UsesController
+        public async Task<ActionResult> Index()
+        {
+            var users = await _context.Users.ToListAsync();
+            return View(users);
+        }
+
+        // GET: UsesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UsesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult>Create(ApplicationUser user)
+        {
+            try
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                ApplicationUser registeduser = new();
+                registeduser.FirstName = user.FirstName;
+                registeduser.UserName = user.UserName;
+                registeduser.MiddleName = user.MiddleName;
+                registeduser.LastName = user.LastName;  
+                registeduser.NormalizedUserName = user.NormalizedUserName;
+                registeduser.Email = user.Email;
+                registeduser.EmailConfirmed = true;
+                registeduser.Gender = user.Gender;
+                registeduser.Country = user.Country;
+                registeduser.City = user.City;
+                registeduser.PhoneNumber = user.PhoneNumber;
+
+                var result = await _userManager.CreateAsync(registeduser, user.PasswordHash);
+                if (result.Succeeded)
+                {
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+
+
+            }
+            catch(Exception ex)
+            {
+                return View();
+            }
+        }
+
+        // GET: UsesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UsesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UsesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ HelpDesk/Data/Migrations/20241024200656_UpdateAuditTrailColumns.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HelpDesk.Data.Migrations
+{
+    public partial class UpdateAuditTrailColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Remove the primary key constraint
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AuditTrails",
+                table: "AuditTrails");
+
+            // Drop the old 'Id' column
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "AuditTrails");
+
+            // Recreate 'Id' column as an identity column
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "AuditTrails",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            // Re-add the primary key constraint on 'Id'
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AuditTrails",
+                table: "AuditTrails",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Remove the primary key constraint
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AuditTrails",
+                table: "AuditTrails");
+
+            // Drop the new 'Id' column
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "AuditTrails");
+
+            // Recreate the old 'Id' column as a string
+            migrationBuilder.AddColumn<string>(
+                name: "Id",
+                table: "AuditTrails",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            // Re-add the primary key constraint
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AuditTrails",
+                table: "AuditTrails",
+                column: "Id");
+        }
+    }
+}
