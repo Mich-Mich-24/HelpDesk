c3f43d5980f20a35db587ed2643d819124fc9fea
Mich-Mich-24 <michelledibucks24@gmail.com>
2025-03-21 14:49:24 +0200

    .

---
+++ HelpDesk/wwwroot/js/treeview-init.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $("#treeData").hummingbird();
+
+    $("#expandAll").click(function () {
+        $("#treeData").hummingbird("expandAll");
+    });
+    $("#collapseAll").click(function () {
+      $("#treeData").hummingbird("collapseAll");
+    });
+});
